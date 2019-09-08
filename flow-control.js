2690998b746fdf583e098334e41a44d346d3ec7d
kim <krmciver@mail.sfsu.edu>
2019-09-08 20:10:55 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,12 +1,14 @@
 function basicTeenager(age) {
-
+  if (age >= 13 && age <= 19) {return "You are a teenager!"} else { return undefined}
 }
 
 function teenager(age) {
+  if (age >= 13 && age <=19) { return "You are a teenager!"}
 
 }
 
 function ageChecker(age) {
+  if ()
 
 }
 
